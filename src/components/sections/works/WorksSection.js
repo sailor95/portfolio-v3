c55a750807c0c4fc17734d3b3f4f7bb6f586700f
David Huang <bugman195@gmail.com>
2019-12-24 00:28:26 +0800

    Restructure responsive Card Grid

--- src/components/sections/works/WorksSection.js
+++ src/components/sections/works/WorksSection.js
@@ -7,6 +7,7 @@ const tempWorksData = [
   {
     id: 1,
     name: 'This Portfolio',
+    des: 'This SPA web page.',
     stacks: ['React', 'SCSS', 'Azure'],
     demoLink: "",
     github: ""
@@ -14,6 +15,7 @@ const tempWorksData = [
   {
     id: 2,
     name: 'Hacker News SPA',
+    des: 'An SPA fetching Hacker News API and sort into list view.',
     stacks: ['React', 'Redux', 'CSS Module', 'GCP'],
     demoLink: "",
     github: ""
@@ -21,6 +23,31 @@ const tempWorksData = [
   {
     id: 3,
     name: 'Unit Test Project',
+    des: 'An unit test porject using Jest and Enzyme to simulate front-end behavior.',
+    stacks: ['Jest', 'Enzyme', 'ESLint'],
+    demoLink: "",
+    github: ""
+  },
+  {
+    id: 1,
+    name: 'This Portfolio',
+    des: 'This SPA web page.',
+    stacks: ['React', 'SCSS', 'Azure'],
+    demoLink: "",
+    github: ""
+  },
+  {
+    id: 2,
+    name: 'Hacker News SPA',
+    des: 'An SPA fetching Hacker News API and sort into list view.',
+    stacks: ['React', 'Redux', 'CSS Module', 'GCP'],
+    demoLink: "",
+    github: ""
+  },
+  {
+    id: 3,
+    name: 'Unit Test Project',
+    des: 'An unit test porject using Jest and Enzyme to simulate front-end behavior.',
     stacks: ['Jest', 'Enzyme', 'ESLint'],
     demoLink: "",
     github: ""
--- src/components/sections/works/work-boxes-list/work-box/WorkBox.js
+++ src/components/sections/works/work-boxes-list/work-box/WorkBox.js
@@ -7,26 +7,30 @@ import { ReactComponent as GitHubIcon } from 'res/github.svg';
 import { ReactComponent as PlayIcon } from 'res/play-button.svg';
 
 const WorkBox = ({ workData }) => {
-  const renderStacks = arr => arr.map(a => (
-    <div>{a}</div>
-  ));
+  const renderStacks = arr => arr.join(', ');
 
   return (
-    <Col sm={12} md={4}>
-      <Card className="workbox">
-        <Card.Header>
-          {workData.name}
-        </Card.Header>
+    <Col xs={12} md={6} xl={4} className="mb-5">
+      <Card className="workbox h-100">
+        <Card.Img className="p-3" variant="top" src={require("res/browser.svg")} height="80" width="100" />
         <Card.Body>
-          {renderStacks(workData.stacks)}
+          <Card.Title>
+            {workData.name}
+          </Card.Title>
+          <Card.Text>
+            {workData.des}
+          </Card.Text>
+          <Card.Text className="blockquote-footer">
+            {renderStacks(workData.stacks)}
+          </Card.Text>
         </Card.Body>
-        <Card.Footer className="d-flex justify-content-around">
-          <Button variant="secondary">
-            <PlayIcon className="play-icon mr-3" />
+        <Card.Footer>
+          <Button variant="secondary" className="mr-3">
+            <PlayIcon className="play-icon mr-2" />
             <span>Demo</span>
           </Button>
           <Button variant="secondary">
-            <GitHubIcon className="github-icon mr-3" />
+            <GitHubIcon className="github-icon mr-2" />
             <span>GitHub</span>
           </Button>
         </Card.Footer>

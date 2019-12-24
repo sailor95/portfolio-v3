c293fdfeab836e7a13da1e0e0893c1ca6e772daa
David Huang <bugman195@gmail.com>
2019-12-24 23:14:11 +0800

    Add class name to avoid global assign CSS prop

--- src/components/sections/landing/links-box/LinksBox.js
+++ src/components/sections/landing/links-box/LinksBox.js
@@ -8,6 +8,7 @@ import { ReactComponent as MediumLogo } from 'res/medium.svg';
 const LinksBox = () => (
   <>
     <a
+      className="linkBoxAnchor"
       href="https://github.com/sailor95"
       target="_blank"
       rel="noopener noreferrer"
@@ -15,6 +16,7 @@ const LinksBox = () => (
       <GitHubLogo className="linksBoxSvg" alt="github" />
     </a>
     <a
+      className="linkBoxAnchor"
       href="https://www.linkedin.com/in/tsung-wei-huang-217561179/"
       target="_blank"
       rel="noopener noreferrer"
@@ -22,6 +24,7 @@ const LinksBox = () => (
       <LinkedInLogo className="linksBoxSvg" alt="linkedin" />
     </a>
     <a
+      className="linkBoxAnchor"
       href="https://medium.com/sailor-spaceship"
       target="_blank"
       rel="noopener noreferrer"

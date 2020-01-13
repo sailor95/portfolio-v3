849310687e7357a17a74120a7563da6a1bd04fe0
David Huang <bugman195@gmail.com>
2020-01-13 23:11:36 +0800

    Add smooth scroll to button

--- src/components/sections/landing/check-out-arrow/CheckOutArrow.js
+++ src/components/sections/landing/check-out-arrow/CheckOutArrow.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-scroll';
 
 import './CheckOutArrow.scss';
 import { ReactComponent as DownArrow } from 'res/down-arrow.svg';
 
 const CheckOutArrow = () => (
-  <div className="check-out-box">
+  <Link className="check-out-box" to="works-section" smooth={true} duration={500}>
     <span className="check-out-box__des">Check out my works</span>
     <DownArrow className="check-out-box__down-arrow" alt="down-arrow" />
-  </div>
+  </Link>
 );
 
 export default CheckOutArrow;
--- src/components/sections/works/WorksSection.js
+++ src/components/sections/works/WorksSection.js
@@ -5,7 +5,7 @@ import WorkBoxesList from './work-boxes-list/WorkBoxesList';
 import { tempWorksData } from 'constants/ui/uiConstants';
 
 const WorksSection = () => (
-  <div className="works-section">
+  <div className="works-section" id="works-section">
     <div className="works-section__header">
       My Works
     </div>

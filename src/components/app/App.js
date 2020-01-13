92f6558010737a940b7ce6244bc4b88bd0e5bbff
David Huang <bugman195@gmail.com>
2020-01-13 21:10:58 +0800

    Fix typo & delete extra file

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 import './App.scss';
 import LandingSection from '../sections/landing/LandingSection';
-import WorksSction from '../sections/works/WorksSection';
+import WorksSection from '../sections/works/WorksSection';
 
 const App = () => {
   return (
     <div className="app container-fluid">
       <LandingSection />
-      <WorksSction />
+      <WorksSection />
     </div>
   );
 };

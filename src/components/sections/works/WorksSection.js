d5123c791d5fc50211869e50cb0a1ef9f1802e9a
David Huang <bugman195@gmail.com>
2020-01-14 12:26:52 +0800

    Create UI consts for works section

--- src/components/sections/works/WorksSection.js
+++ src/components/sections/works/WorksSection.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
 import './WorksSection.scss';
+import { WORKS } from 'constants/ui/sections/worksConsts';
 import WorkBoxesList from './work-boxes-list/WorkBoxesList';
 import { tempWorksData } from 'constants/ui/mockDataConsts';
 
 const WorksSection = () => (
   <div className="works-section" id="works-section">
-    <div className="works-section__header">
-      My Works
-    </div>
+    <div className="works-section__header">{WORKS.header}</div>
 
     <WorkBoxesList worksData={tempWorksData} />
   </div>
---
+++ src/constants/ui/sections/worksConsts.js
@@ -0,0 +1,3 @@
+export const WORKS = {
+  header: "My Works"
+};

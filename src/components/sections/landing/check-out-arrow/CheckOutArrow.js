b05aaf09737d15b258210bbd09047e45f4b37089
David Huang <bugman195@gmail.com>
2020-01-14 12:02:17 +0800

    Create UI consts for landing section

--- src/components/sections/landing/check-out-arrow/CheckOutArrow.js
+++ src/components/sections/landing/check-out-arrow/CheckOutArrow.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Link } from 'react-scroll';
 
 import './CheckOutArrow.scss';
+import { LANDING } from 'constants/ui/sections/landingConsts';
 import { ReactComponent as DownArrow } from 'res/down-arrow.svg';
 
 const CheckOutArrow = () => (
   <Link className="check-out-box" to="works-section" smooth={true} duration={500}>
-    <span className="check-out-box__des">Check out my works</span>
+    <span className="check-out-box__des">{LANDING.checkout.des}</span>
     <DownArrow className="check-out-box__down-arrow" alt="down-arrow" />
   </Link>
 );
---
+++ src/constants/ui/sections/landingConsts.js
@@ -0,0 +1,9 @@
+export const LANDING = {
+  header: {
+    main: "I am David Huang",
+    sub: "Web full-stack / Tech geek / All sorts of arts"
+  },
+  checkout: {
+    des: "Check out my works"
+  }
+};
